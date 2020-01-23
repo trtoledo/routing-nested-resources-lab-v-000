6af7ffe6d67fca1c184bdbd794a43312e1b23387
Tomas Toledo <trtoledo1977@gmail.com>
2020-01-23 00:02:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :songs, only: [:show, :index]
   end
 
-  resources :songss, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'songs#index'
 end

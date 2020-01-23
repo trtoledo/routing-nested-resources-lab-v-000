28ffeace7d99a06d5a2b884a22f7c0d575b8b322
Tomas Toledo <trtoledo1977@gmail.com>
2020-01-23 01:06:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @artist = Artist.find(:artist_id).songs
+      @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         flash[:alert] = "Artist not found"
         redirect_to artists_path
@@ -14,7 +14,16 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        flash[:alert] =  "Song not found"
+        redirect_to artist_songs_path(@artist)
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
